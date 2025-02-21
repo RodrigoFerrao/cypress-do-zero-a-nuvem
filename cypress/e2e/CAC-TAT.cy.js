9c7f72ada4b2e0b5a9663ca98e2f06d3935921a7
RodrigoFerrao <rodrigo.ferrao@ufba.br>
2025-02-21 22:01:55 +0000

    Task02 - add beforeEach and first exercise

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,6 +1,24 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
-  it('Verifica o título da aplicação', () => {
+
+  beforeEach(() => {
     cy.visit('./src/index.html')
+  });
+  
+  it('Verifica o título da aplicação', () => { 
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-  })
-})
+  });
+
+  it('Preenche os campos obrigatórios e envia o formulário', ()=> {
+    cy.get('#firstName').type('Rodrigo')
+    cy.get('#lastName').type('Ferrão')
+    cy.get('#email').type('teste@email.com')
+    cy.get('#open-text-area').type('Mensagem de feedback um pouco longa, porém com delay 0', {delay : 0})
+    cy.get('button[type="submit"]').click()
+    cy.get('span[class="success"]').should('be.visible')
+  });
+
+});
+
+
+
+
